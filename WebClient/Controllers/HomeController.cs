269dddb2f34f8fdf2c7f957bc814c46a94f622cf
Artyom Popov <rendre21@gmail.com>
2022-02-20 17:19:11 +0700

    WebClient init

---
+++ WebClient/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebClient.Models;
+
+namespace WebClient.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Index()
+        {
+            return "123";
+        }
+    }
+}
